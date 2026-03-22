25af031b25d3a72028bbb331710d6fc91b8a61de
Zynomon aelius <92979020+zynomon@users.noreply.github.com>
2026-03-22 23:09:38 +0600

    Update lxqt.sh

--- lxqt.sh
+++ lxqt.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-echo "Looks like you have installed lxqt.sh for now wait we are testing the best combination"
+echo "Looks like you have installed lxqt.sh for now wait we are testing the best combination /n you can clone this repo for lxqt after few days as for now see kde.sh "
